a05e9b6c2036c10c9a9690ec26500f3fe01bda7b
Sap <162757246+Scaroontop@users.noreply.github.com>
2025-03-22 13:46:34 -0400

    Update verification.js

--- verification.js
+++ verification.js
@@ -36,8 +36,8 @@ const extensions = {
     "Borderless Classroom Student (2nd ID)": "chrome-extension://apchgbgnimojffnkddiigiekiooeieno/pages/blockPage.html",
     "LockDown Browser AP Classroom Edition": "chrome-extension://djpknfecbncogekjnjppojlaipeobkmo/assets/images/icon_128.png",
     "Lugus School": "chrome-extension://eoobggamkobbcpiojefejfglbfcacgca/assets/images/icon_128.png",
-    "Goguardian license": "chrome-extension://iebjhfinbefifbikdchdnodkdickdghe/background.html",
-    "Clever plus": "chrome-extension://dikiaagfielfbnbbopidjjagldjopbpa/js/credential_form.html",
+    "Goguardian license": "chrome-extension://iebjhfinbefifbikdchdnodkdickdghe/manifest.json",
+    "Clever plus": "chrome-extension://dikiaagfielfbnbbopidjjagldjopbpa/manifest.json"
 };
 
 // Configuration
@@ -47,9 +47,9 @@ const CONFIG = {
     USER_DATA_KEY: "verification-user-data",
     VERIFICATION_PAGE: "/index.html",
     DEFAULT_REDIRECT: "./h.html",
-    CURRENT_USER: "Scaroontop",
+    CURRENT_USER: "Person",
     CURRENT_UTC_TIME: "2025-03-22 17:10:01",
-    REQUIRED_EXTENSIONS: 2
+    REQUIRED_EXTENSIONS: 5
 };
 
 function saveUserData(verificationStatus) {
